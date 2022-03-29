59ebc7c7d88249b3539bf8efabe46dee33fc2d8e
Luke Driscoll <ldriscoll@chapman.edu>
2022-03-29 02:06:12 -0700

    Fine tuning code Adding in comments and removing useless code

--- fetchModule.js
+++ fetchModule.js
@@ -9,6 +9,7 @@ btns.forEach(function (btn) {
         const styles = e.currentTarget.classList;
         //Setting Up Fetch
         var url = "https://jikan1.p.rapidapi.com/genre/anime/";
+        //Adds the URL for the different genres
         if(styles.contains("action"))
         {
             url += "1/1"
@@ -37,17 +38,6 @@ btns.forEach(function (btn) {
             url += "10/1";
 
         }
-        //Basic formatting for the genres
-        // 1 Action
-        // 2 Adventure
-        // 3
-        // 4 Comedy
-        // 5
-        // 6
-        // 7 Mystery
-        // 8 Drama
-        // 9
-        // 10 Fantasy
 
         fetch(url, {
             "method": "GET",
@@ -56,29 +46,27 @@ btns.forEach(function (btn) {
                 "x-rapidapi-key": "07cb6ccf43mshc70113667ee75b6p111a42jsn46be7a8b4a23"
             }
         })
-                .then(res => res.json())
+        .then(res => res.json())
         .then(response => {
-            //console.log(response);
+            
             Object.entries(response).forEach(([key, value]) => {
+
                 //Gets the anime section of the JSON
                 if(JSON.stringify(key).includes('anime')){
                     //Loops through all the animes
-                    //Key 2 is the number of anime
                     //For Each Loop to number from 0-99 which is the random anime in that genre
                     const randomPosition = Math.floor(Math.random() *100);
                     Object.entries(value).forEach(([key2, value2]) => {
+                        //Get the anime at the random position
                         if(JSON.stringify(key2).includes(randomPosition.toString())){
-                            //Look through properties of the anime and get Name(2), image(3) and Summary(4)
+                            //Look through properties of the anime and get Name(2), image(3), Summary(4) and Rating(15)
                             var counter = 0;
-                            var returnString = "";
                             Object.entries(value2).forEach(([key3, value3]) => {
                                 if(counter == 2){
                                     animeName.textContent = (`${value3}`);
                                 }else if(counter == 3){
-                                    console.log(`${value3}`);
                                     animeImage.src = (`${value3}`);
                                     animeImage.alt = (`${value3}`);
-                                    console.log(animeImage.src);
                                 }else if(counter == 4){
                                     animeDescription.textContent = (`${value3}`);
                                 }else if(counter == 15){
@@ -88,7 +76,6 @@ btns.forEach(function (btn) {
                                 counter++;
                             });
                         }
-                        
                     });
                 }
             });
