4aa73e81f32d09b5ca1d8c91cb10c01aed9e2706
Luke Driscoll <ldriscoll@chapman.edu>
2022-04-26 02:10:58 -0700

    Added Dictionary for genre and Modern JS
    Changed var to let
    Made dictionary for easy addition of genres

--- App.js
+++ App.js
@@ -9,36 +9,24 @@ btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         const styles = e.currentTarget.classList;
         //Setting Up Fetch
-        var url = "https://jikan1.p.rapidapi.com/genre/anime/";
+        let url = "https://jikan1.p.rapidapi.com/genre/anime/";
+
         //Adds the URL for the different genres
-        if(styles.contains("action"))
-        {
-            url += "1/1"
-        }
-        else if(styles.contains("adventure"))
-        {
-            url += "2/1";
-
-        }else if(styles.contains("comedy"))
-        {
-            url += "4/1";
-
-        }
-        else if(styles.contains("mystery"))
-        {
-            url += "7/1";
-
-        }
-        else if(styles.contains("drama"))
-        {
-            url += "8/1";
-
-        }
-        else if(styles.contains("fantasy"))
-        {
-            url += "10/1";
-
-        }
+        let genreToUrlDict = {
+            "action": "1",
+            "adventure": "2",
+            "comedy": "4",
+            "mystery": "7",
+            "drama": "8",
+            "fantasy": "10"
+        };
+        Object.keys(genreToUrlDict).forEach(function(key) {
+            if(styles.contains(key))
+            {
+                url += genreToUrlDict[key] + "/1"
+            }
+        });
+        
         //Waiting Image
         animeImage.src = "http://phette23.github.io/speed-is-a-feature/img/loadingBar.gif";
         animeImage.alt = "http://phette23.github.io/speed-is-a-feature/img/loadingBar.gif";
@@ -70,7 +58,7 @@ btns.forEach(function (btn) {
                         if(JSON.stringify(key2).includes(randomPosition.toString())){
 
                             //Look through properties of the anime and get Name(2), image(3), Summary(4) and Rating(15)
-                            var counter = 0;
+                            let counter = 0;
                             Object.entries(value2).forEach(([key3, value3]) => {
                                 if(counter == 2){
                                     animeName.textContent = (`${value3}`);
